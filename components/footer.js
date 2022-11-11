ecd5d13d9fe8deda51ba8f793dbb72ebb4c2a08f
nicoescudero <nicolas.escudero00@gmail.com>
2022-11-11 18:50:50 -0300

    edit: footer

---
+++ components/footer.js
@@ -0,0 +1,9 @@
+import style from "../styles/Home.module.css";
+
+export default function Footer (){
+  return (
+  <footer className={style.footer}>
+    <p>Copyright © nicoescudero</p>
+  </footer>
+  );
+}
