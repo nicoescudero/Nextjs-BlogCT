baff1ea62cbc3f4de3af6f90868b32e1c3a3d44d
nicoescudero <nicolas.escudero00@gmail.com>
2022-11-11 18:51:13 -0300

    add: btn_coffee

---
+++ components/btn_coffee.js
@@ -0,0 +1,11 @@
+import style from '../styles/Post.module.css';
+
+export default function ButtonCoffee() {
+  return (
+    <div className={style.btn_coffee}>
+      <a href="https://www.buymeacoffee.com/nicoDeveloper">
+        Buy me a Coffe
+      </a>
+    </div>
+  );
+};
