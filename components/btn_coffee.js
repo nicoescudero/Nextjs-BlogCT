ce8ee04a6b8c457e81f2807a07114e78fcaa686a
nicoescudero <nicolas.escudero00@gmail.com>
2022-11-12 18:10:05 -0300

    fix: button coffee

--- components/btn_coffee.js
+++ components/btn_coffee.js
@@ -1,10 +1,12 @@
 import style from '../styles/Post.module.css';
+import Image from 'next/image';
+import coffee from '../public/bmc-button.png';
 
 export default function ButtonCoffee() {
   return (
     <div className={style.btn_coffee}>
       <a href="https://www.buymeacoffee.com/nicoDeveloper">
-        Buy me a Coffe
+        <Image src={coffee} alt="buy me a coffe widget" />
       </a>
     </div>
   );
